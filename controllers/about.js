9d56f8b5a5be347820b9491d39bda7733290ada4
Sam-Mathias <samuel.adam.mathias@gmail.com>
2026-02-12 16:13:27 +0000

    Add Models and Views

--- controllers/about.js
+++ controllers/about.js
@@ -1,10 +1,18 @@
 'use strict';
+
 import logger from "../utils/logger.js";
+import empStore from "../models/employee.js";
 
 const about = {
   createView(request, response) {
     logger.info("About page loading!");
-    response.send('About the Playlist app');   
+    const viewData = {
+      title: "About the Playlist app!",
+      employee: empStore.getEmpInfo()
+    }; 
+
+    logger.info(viewData.employee)
+    response.render('about', viewData)
   },
 };
 export default about;
---
+++ models/employee.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import logger from '../utils/logger.js';
+import JsonStore from './json-store.js';
+
+const empStore = {
+
+  store: new JsonStore('./models/employee.json', { employee: {} }),
+  collection: 'employee',
+  
+  getEmpInfo() {
+    return this.store.findAll(this.collection);
+  },
+
+};
+
+export default empStore;
