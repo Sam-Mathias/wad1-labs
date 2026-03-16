c699c4859795eeb1d592d22fed6464eaf743a9b8
Sam-Mathias <samuel.adam.mathias@gmail.com>
2026-03-16 11:53:59 +0000

    Completed Delete Functions Lab

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,6 +2,7 @@
 
 import logger from "../utils/logger.js";
 import playlistStore from "../models/playlist-store.js";
+import { v4 as uuidv4 } from 'uuid';
 
 const dashboard = {
   createView(request, response) {
@@ -15,6 +16,23 @@ const dashboard = {
     logger.debug(viewData.playlists);
     
     response.render('dashboard', viewData);
+  },
+
+  addPlaylist(request, response) {
+    const newPlayList = {
+      id: uuidv4(),
+      title: request.body.title,
+      songs: [],
+    };
+    playlistStore.addPlaylist(newPlayList);
+    response.redirect('/dashboard');
+  },
+  
+  deletePlaylist(request, response) {
+    const playlistId = request.params.id;
+    logger.debug(`Deleting Playlist ${playlistId}`);
+    playlistStore.removePlaylist(playlistId);
+    response.redirect("/dashboard");
   },
 };
 
