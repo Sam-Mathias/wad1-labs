1878f7dd9aa36db05be80bf3a53ae0f362dc8e90
Sam-Mathias <samuel.adam.mathias@gmail.com>
2026-04-22 16:31:49 +0100

    Image upload functionality added

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -24,9 +24,20 @@ const playlistStore = {
   removeSong(id, songId) {
     this.store.removeItem(this.collection, id, this.array, songId);
   },
-  removePlaylist(id) {
+    async removePlaylist(id, response) {
     const playlist = this.getPlaylist(id);
+
+    if (playlist.picture && playlist.picture.public_id) {
+      try {
+        await this.store.deleteFromCloudinary(playlist.picture.public_id);
+        logger.info("Cloudinary image deleted");
+      } catch (err) {
+        logger.error("Failed to delete Cloudinary image:", err);
+      }
+    }
+
     this.store.removeCollection(this.collection, playlist);
+    response();
   },
   editSong(id, songId, updatedSong) {
     this.store.editItem(this.collection, id, songId, this.array, updatedSong);
@@ -46,6 +57,17 @@ const playlistStore = {
       this.collection,
       (playlist => playlist.userid === userid && playlist.title.toLowerCase().includes(search.toLowerCase())))
   }, 
+
+    async addPlaylist(playlist, file, response) {
+    try {
+      playlist.picture = await this.store.addToCloudinary(file);
+      this.store.addCollection(this.collection, playlist);
+      response();
+    } catch (error) {
+      logger.error("Error processing playlist:", error);
+      response(error);
+    }
+  },
 };
 
 export default playlistStore;
